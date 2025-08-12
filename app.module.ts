6982479fa4230a34879473273f0c942f039b5e2e
captianam <139987305+captianam@users.noreply.github.com>
2025-08-12 08:14:49 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { StudentsComponent } from './students/students.component'; // ✅
+import { StudentsComponent } from './students/students.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent  // ✅ must be here
+    StudentsComponent  
   ],
   imports: [
     BrowserModule,
